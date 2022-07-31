7be642ddc7724ee1800d33ec21463edf0e02be4d
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-07-31 16:57:36 +0600

    practice problem added

---
+++ practice-problem.js
@@ -0,0 +1,90 @@
+
+// -------------Problem Number 1 ---------------//
+
+var givenAmount = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var totalSpend = applePrice + orangePrice;
+// result ok 
+// console.log(totalSpend); 
+
+var remainingAmount = givenAmount - totalSpend;
+
+console.log(remainingAmount);
+
+
+// ----------------Problem number 2 --------------//
+
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalSubject = 5;
+
+var totalMarks = math + biology + chemistry + physics + bangla;
+
+// result ok
+// console.log(totalMarks); 
+
+var averageMarks = totalMarks / totalSubject;
+// result ok 
+// console.log(averageMarks); 
+
+
+averageMarks = averageMarks.toFixed(2);
+console.log(averageMarks);
+
+// ----------------Problem number 3 --------------//
+
+var firstPart = 'I am going to be';
+var lastPart = 'an awesome web developr';
+
+var theSentence = firstPart + ' ' + lastPart;
+console.log(theSentence);
+
+
+// ----------------Problem number 4 --------------//
+
+var mainAmount = 119;
+var divider = 5;
+
+var remainder = mainAmount % divider;
+console.log(remainder);
+
+
+// ------------Practice Challenge -------------------//
+
+Var price = 33 ------------প্রথম V ছোট হাতের হবে।
+
+var name - Shabana------------ভেলু এর আগে = চিহ্ন হবে এবং ভেলু টা কোটেশন এর ভিতরে ঢুকবে ।
+
+var boxName = ‘Cocola; ------------- ভেলু টা কোটেশন এর ভিতরে ঢুকবে ।
+
+var 88_price = 34; ------------------ভেরিয়েবল এর নাম দেওয়ার ক্ষেত্রে আগে নাম্বার ইউজ করা যাবেনা ।
+
+var enum = -1; -------------------- enum হচ্ছে একটা জাভাস্ক্রিপ্ট এর রিজার্ভ কিওয়ার্ড যার কারনে এটা নাম হিসেবে ব্যবহারযোগ্য নয় ।
+
+var _$box’78 = ‘Monika’; ------------- ভেরিয়েবল এর নামের মধ্য নাম্বার এর আগে কোটেশন হবেনা ।
+
+var home-address = “kochu khet”; নামের মাঝে - হবেনা _ হবে ।
+
+
+// -----------------Practice Challenge Answer --------------------//
+
+var price = 33;
+
+var name = 'Shabana';
+
+var boxName = 'Cocola';
+
+var price_88 = 34;
+
+var enam = -1;
+
+var $box78 = 'Monika';
+
+var home_address = 'kochu khet';
+
