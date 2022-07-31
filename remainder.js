9334dc31d1754a7f4345658da5e45b0146f0431b
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-07-31 15:53:35 +0600

    remainder added

---
+++ remainder.js
@@ -0,0 +1,12 @@
+// ---------difference beetween divider and reminder --------------//
+
+var mangoes = 23;
+var people = 5;
+// devider 
+var perPerson = mangoes / people;
+console.log(perPerson);
+
+// remainder (reminder calls with % symbol)
+
+var remain = mangoes % people;
+console.log(remain);
